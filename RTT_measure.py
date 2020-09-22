0428f9b081886ac285362a5cdc23b43755870856
SYBreloom <772622641@qq.com>
2020-09-22 20:45:14 +0800

    modify the RTT value parsing

--- RTT_measure.py
+++ RTT_measure.py
@@ -8,6 +8,7 @@ import threading
 
 interval = 1
 address = "www.baidu.com"
+RTTs = []
 
 
 def get_RTT():
@@ -16,7 +17,12 @@ def get_RTT():
     for i in res:
         a = re.findall("time=(.+?) ms", i)  # 只有一行会输出 icmp_seq=3 ttl=128 time=54.4 ms
         if len(a) != 0:
-            print(a[0])  # 使用ping-c的时候，只有一个time的匹配，直接输出了
+            try:
+                # RTTs.append(float(a[0]))  # 使用ping-c的时候，只有一个time的匹配，直接输出了
+                print(a[0])
+            except ValueError:
+                print("error when parsing RTT value")
+
     timer = threading.Timer(interval, get_RTT)
     timer.start()
 
